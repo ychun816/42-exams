d8739f24e063c8816cf712e9416814e72afec13a
Yi-Chun LIN <105208178+ychun816@users.noreply.github.com>
2025-01-28 01:55:02 +0000

    rev exam4

--- exam4/microshell.c
+++ exam4/microshell.c
@@ -1,98 +1,158 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
-
-
-//write error
-void    err_msg(char *str)
+//fd_msg
+void fd_msg(char *str)
 {
-    while (*str)
-        write(2, str++, 1);
+	while (*str)
+		write(2, str++, 1);
 }
 
-//ft_cd
-int ft_cd(int ac, char *av[])
+//cd
+//i !=2 -> err
+//chdir fail -> err
+int cd(char *av[], int i)
 {
-    //input has to be 2
-    if (ac != 2)
-        return (err_msg("error: cd: bad arguments\n"), 1);
-    //if cd fail
-    if (chdir(av[1]) == -1)
-    {
-        err_msg("error: cd: cannot change directory to ");
-        err_msg(av[1]);
-        err_msg("\n");
-        return (1);
-    }
+	if (i != 2)
+		return (fd_msg("error: cd: wrong argument\n"), 1);
+	if (chdir(av[1]) == -1)
+	{
+		fd_msg("error: cd: cannot change directory to ");
+		fd_msg("av[i]");
+		fd_msg("\n");
+		return (1);
+	}
+	return (0);
 }
-
-//////////WAIT FOR PRACTICE //////////
-// Function to set pipe
-// end == 1 sets stdout to act as write end of our pipe
-// end == 0 sets stdin to act as read end of our pipe
+	
+//set_pipe
+//fail -> exit(1)
 void set_pipe(int has_pipe, int *fd, int end)
 {
 	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		err("error: fatal\n"), exit(1);
+		fd_msg("error: fatal\n"), exit(1);
 }
 
-// Function to execute a command in a child process
-int exec(char **argv, int i, char **envp)
+//exec
+//no pipe, just cd -> call cd
+//has pipe, pipe fail -> err
+//fork fail -> err
+//in child process (!pid) loop 
+//child process stop -> back to mother process -> waitpid, set_pipe
+//return (WIFEXITED(status) && WEXITSTATUS(status));
+int exec(char *av[], int i, char *env[])
 {
-	int has_pipe, fd[2], pid, status;
+	int has_pipe;
+	int fd[2];
+	int pid;
+	int status;
 
-	// Check if the command includes a pipe
-	has_pipe = argv[i] && !strcmp(argv[i], "|");
-
-	// If the command is 'cd', execute it and no pipe
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return cd(argv, i);
-
-	// If the command includes a pipe and creating the pipe fails, print error and exit
+	has_pipe = av[i] && !strcmp(av[i], "|");
+	if (!has_pipe && !strcmp(*av, "cd"))
+		return (cd(av, i));
 	if (has_pipe && pipe(fd) == -1)
-		err("error: fatal\n"), exit(1);
-
-	// Fork the process and if the fork fails, print error and exit
+		fd_msg("error: fatal\n"), exit(1);
 	if ((pid = fork()) == -1)
-	err("error: fatal\n"), exit(1);
+		fd_msg("error: fatal\n"), exit(1);
 	if (!pid)
 	{
-		argv[i] = 0;
-		// If the command includes a pipe, set write end of pipe, if it fail print error and exit
+		av[i] = 0; //set NULL
 		set_pipe(has_pipe, fd, 1);
-		// If the command is 'cd', exit after executing it
-		if (!strcmp(*argv, "cd"))
-			exit(cd(argv, i));
-		// Execute the command
-		execve(*argv, argv, envp);
-		// If executing the command fails, print error and exit
-		err("error: cannot execute "), err(*argv), err("\n"), exit(1);
+		if (!strcmp(*av, "cd"))
+			exit(cd(av, i));
+		execve(*av, av, env);
+		fd_msg("error: cannot execute "), fd_msg("*av"), fd_msg("\n"), exit(1);
 	}
-
-	// Wait for the child process to finish
 	waitpid(pid, &status, 0);
-	// If the command includes a pipe, set write end of pipe, if it fail print error and exit
 	set_pipe(has_pipe, fd, 0);
-	// Return the exit status of the child process
-	return WIFEXITED(status) && WEXITSTATUS(status);
+	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
-int main(int, char **argv, char **envp)
+//main
+//loop av[i]
+//|, ; -> i++;
+int main(int ac, char *av[], char *env[])
 {
-	int	i = 0, status = 0;
+	int status = 0;
+	int i = 0;
 
-	// Loop through each following argument
-	while (argv[i])
+	(void)ac;
+	while (av[i])
 	{
-		// Move the pointer to the next argument after the last delimeter / first argument
-		argv += i + 1;
+		av += i + 1;
 		i = 0;
-		// It skips past any semicolons (;) or pipe (|) separators
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
-		// If there are arguments (between separators), execute them
 		if (i)
-			status = exec(argv, i, envp);
+			status = exec(av, i, env);
 	}
-	// Returns the last command's exit status
-	return status;
+	return (status);
 }
+
+/* ------------------------------- TEST --------------------------------------
+
+						> Commands that should work <
+
+Simple command execution:
+./a.out /bin/echo Hello, World!
+Expected output: Hello, World!
+
+--
+Change directory with cd:
+./a.out cd /tmp ";" /bin/pwd
+Expected output: /tmp
+
+--
+Piping between commands:
+./a.out /bin/echo Hello, World! "|" /usr/bin/grep World
+Expected output: Hello, World!
+
+--
+Multiple pipes:
+./a.out /bin/echo Hello, World! "|" /usr/bin/tr ' ' '\n' "|" /usr/bin/grep Hello
+Expected output: Hello
+
+--
+Multiple commands with pipes and separators:
+./a.out /bin/echo Hello ";" /bin/echo World "|" /usr/bin/grep o
+Expected output:
+Hello
+World
+
+--
+Chaining commands with ;:
+./a.out /bin/echo First command ";" /bin/echo Second command
+Expected output:
+First command
+Second command
+
+--
+Complex pipeline with mixed commands:
+./a.out /bin/echo "microshell is cool" "|" /usr/bin/grep microshell "|" /usr/bin/wc -c ";" /bin/echo "Done"
+Expected output:
+19 (or any number of characters counted)
+Done
+
+"microshell": 10 characters
+" is cool": 8 characters (space before "is" is also counted)
+Newline \n: 1 character
+
+						> Commands that shouldn;t work <
+
+Incorrect usage of cd:
+./a.out cd
+Expected output: error: cd: bad arguments
+--
+
+Nonexistent directory with cd:
+./a.out cd /nonexistent_directory
+Expected output: error: cd: cannot change directory to /nonexistent_directory
+
+--
+Nonexistent executable:
+./a.out /nonexistent_command
+Expected output: error: cannot execute /nonexistent_command
+
+*/

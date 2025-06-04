a56526f28e44cb03c65394535e33d0f50c8931c3
ychun816 <good62226@gmail.com>
2025-06-04 18:58:10 +0200

    starting ex2, added implement notes

---
+++ exam5/ex02/main.cpp
@@ -0,0 +1,32 @@
+
+
+
+int main() {
+  Warlock richard("Richard", "foo");
+  richard.setTitle("Hello, I'm Richard the Warlock!");
+  BrickWall model1;
+
+  Polymorph* polymorph = new Polymorph();
+  TargetGenerator tarGen;
+
+  tarGen.learnTargetType(&model1);
+  richard.learnSpell(polymorph);
+
+  Fireball* fireball = new Fireball();
+  richard.learnSpell(fireball);
+
+  ATarget* wall = tarGen.createTarget("Inconspicuous Red-brick Wall");
+
+  richard.introduce();
+  richard.launchSpell("Polymorph", *wall);
+  richard.launchSpell("Fireball", *wall);
+}
+
+/*
+Expected output:
+Richard: This looks like another boring day.
+Richard: I am Richard, Hello, I'm Richard the Warlock!!
+Inconspicuous Red-brick Wall has been turned into a critter!
+Inconspicuous Red-brick Wall has been burnt to a crisp!
+Richard: My job here is done!
+*/

d2dd39125a8b221463b503678df0b138a028e686
ychun816 <good62226@gmail.com>
2025-06-08 20:37:54 +0200

    ex00 finished, cleared some concepts

---
+++ exam5/practice2/ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "Warlock.hpp"
+
+
+int main() {
+  Warlock const richard("Richard", "Mistress of Magma");
+  richard.introduce();
+  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+
+  Warlock* jack = new Warlock("Jack", "the Long");
+  jack->introduce();
+  jack->setTitle("the Mighty");
+  jack->introduce();
+
+  delete jack;
+  return 0;
+}
+
+/*
+Expected output:
+Richard: This looks like another boring day.
+Richard: I am Richard, Mistress of Magma!
+Richard - Mistress of Magma
+Jack: This looks like another boring day.
+Jack: I am Jack, the Long!
+Jack: I am Jack, the Mighty!
+Jack: My job here is done!
+Richard: My job here is done!
+*/

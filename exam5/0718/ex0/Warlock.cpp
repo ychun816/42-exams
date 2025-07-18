8c7130c7771e1629a0bf8a0cbfb25ebd29c3dbf1
ychun816 <ychun8lin@gmail.com>
2025-07-18 11:57:16 +0200

    rev all

---
+++ exam5/0718/ex0/Warlock.cpp
@@ -0,0 +1,25 @@
+#include "Warlock.hpp"
+
+//<NAME>: This looks like another boring day.
+Warlock::Warlock() : _name(""), _title("")
+{
+    std::cout << _name << ": This looks like another boring day." << std::endl;
+}
+
+Warlock::Warlock(const std::string name, const std::string title) : _name(name), _title(title)
+{
+    std::cout << _name << ": This looks like another boring day." << std::endl;
+}
+
+//<NAME>: My job here is done!
+Warlock::~Warlock()
+{
+    std::cout << _name << ": My job here is done!" << std::endl;
+}
+
+//func
+// <NAME>: I am <NAME>, <TITLE> !
+void Warlock::introduce() const
+{
+    std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
